bbbbea1b8ce2e4fdaa2e6f64c22cb55d7bd4d0e4
osamucaaaa <osamucaaaa@gmail.com>
2022-05-02 17:58:20 -0300

    fix: add functions todolist

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { DataService, Message } from '../services/data.service';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,16 +7,42 @@ import { DataService, Message } from '../services/data.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(private data: DataService) {}
+  taskList = [];
+  taskName = '';
 
-  refresh(ev) {
-    setTimeout(() => {
-      ev.detail.complete();
-    }, 3000);
+  constructor(
+    public navCtrl: NavController,
+    public alertCtrl: AlertController,
+  ) { }
+
+  addTask() {
+    if (this.taskName.length > 0) {
+      const task = this.taskName;
+      this.taskList.push(task);
+      this.taskName = '';
+    }
   }
 
-  getMessages(): Message[] {
-    return this.data.getMessages();
+  deleteTask(index) {
+    this.taskList.splice(index, 1);
   }
 
+  async updateTask(index) {
+    const alert = await this.alertCtrl.create({
+      header: 'Update Task?',
+      message: 'Type in your new task to update.',
+      inputs: [{ name: 'editTask', placeholder: 'Task' }],
+      buttons: [
+        { text: 'Cancel', role: 'cancel' },
+        {
+          text: 'Update',
+          handler: data => {
+            this.taskList[index] = data.editTask;
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 }
